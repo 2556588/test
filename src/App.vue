2800e913a489eac9ebd18738ae243fd427aa513d
2556588 <2537019346@qq.com>
2020-05-21 12:27:27 +0800

    初始化项目

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <my-add></my-add>
+    <my-del></my-del>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import add from './components/add.vue'
+import del from './components/del.vue'
 export default {
-  name: 'App',
+  data() {
+    return {}
+  },
   components: {
-    HelloWorld
+    'my-add': add,
+    'my-del': del
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="less" scoped></style>
---
+++ src/components/del.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>当前的count值：</p>
+    <button>-1</button>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {}
+  },
+  methods: {}
+}
+</script>
